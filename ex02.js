fe99aa3011fa5f24bd64dac890e39c2d6576ec6b
Tomoya <flyingfox193807@gmail.com>
2024-09-27 09:49:56 -0700

    finished ex02.js

--- ex02.js
+++ ex02.js
@@ -7,24 +7,53 @@ Create a function named conditionalSum that will be given an array of numbers an
 */
 
 const conditionalSum = function (values, condition) {
-  if (values.length === 0) {
-    return 0
-  }
-  let sum = 0
-  for (let element of values) {
-    if (condition === "even" && element%2 === 0){
-      sum += element
+  let filtered = values.filter((item) => {
+    if (condition === "even") {
+      return item%2 === 0
     }
-    if (condition === "odd" && element%2 === 1) {
-      sum += element
+    if (condition === "odd") {
+      return item%2 === 1
     }
-  }
-  return sum
-};
+  })
+    // let result = filtered.reduce((sum, now) => {
+  //   return sum + now
+  // }, 0)
+  // return result
+
+  // return filtered.reduce((sum, now ) => {
+  //   return sum + now
+  // }, 0)
+
+  return filtered.reduce((sum, now) => sum + now, 0)
+}
 
 console.log(conditionalSum([1, 2, 3, 4, 5], "even")); // 6
 console.log(conditionalSum([1, 2, 3, 4, 5], "odd")); // 9
 console.log(conditionalSum([13, 88, 12, 44, 99], "even")); // 144
 console.log(conditionalSum([], "odd")); // 0
+    
+
+
+
+// const conditionalSum = function (values, condition) {
+//   if (values.length === 0) {
+//     return 0
+//   }
+//   let sum = 0
+//   for (let element of values) {
+//     if (condition === "even" && element%2 === 0){
+//       sum += element
+//     }
+//     if (condition === "odd" && element%2 === 1) {
+//       sum += element
+//     }
+//   }
+//   return sum
+// };
+
+// console.log(conditionalSum([1, 2, 3, 4, 5], "even")); // 6
+// console.log(conditionalSum([1, 2, 3, 4, 5], "odd")); // 9
+// console.log(conditionalSum([13, 88, 12, 44, 99], "even")); // 144
+// console.log(conditionalSum([], "odd")); // 0
 
 module.exports = conditionalSum;
